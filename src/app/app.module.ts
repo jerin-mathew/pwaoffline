903542599f8813fb5dedf7c02f59ddeeaac7be93
Jerin Mathew <Jerin.Mathew@BNSF.com>
2020-09-15 08:46:29 -0500

    adding initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { FirstPageComponent } from './first-page/first-page.component';
+import { SyncStatusComponent } from './sync-status/sync-status.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstPageComponent,
+    SyncStatusComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/sync-status/sync-status.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sync-status',
+  templateUrl: './sync-status.component.html',
+  styleUrls: ['./sync-status.component.css']
+})
+export class SyncStatusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
